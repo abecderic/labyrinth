257efc8880459f2145bc7f45e76798d58822a068
abecderic <abecderisch@web.de>
2016-10-04 11:57:49 +0200

    Fixes respawn point in labyrinth. Fixes #5

--- src/main/java/com/abecderic/labyrinth/worldgen/LabyrinthWorldProvider.java
+++ src/main/java/com/abecderic/labyrinth/worldgen/LabyrinthWorldProvider.java
@@ -2,12 +2,16 @@ package com.abecderic.labyrinth.worldgen;
 
 import com.abecderic.labyrinth.Labyrinth;
 import com.abecderic.labyrinth.config.Config;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.chunk.IChunkGenerator;
 
+import java.util.Random;
+
 public class LabyrinthWorldProvider extends WorldProvider
 {
+    private Random r = new Random();
     private LabyrinthChunkGenerator gen;
 
     public LabyrinthWorldProvider()
@@ -35,5 +39,16 @@ public class LabyrinthWorldProvider extends WorldProvider
             gen = new LabyrinthChunkGenerator(worldObj);
         }
         return gen;
+    }
+
+    @Override
+    public BlockPos getRandomizedSpawnPoint()
+    {
+        BlockPos spawn = super.getRandomizedSpawnPoint();
+        int x = (int)Math.floor(spawn.getX() / 16D + r.nextGaussian() * 8);
+        int z = (int)Math.floor(spawn.getZ() / 16D + r.nextGaussian() * 8);
+        x = x * 16 + 8;
+        z = z * 16 + 8;
+        return new BlockPos(x, 66, z);
     }
 }
